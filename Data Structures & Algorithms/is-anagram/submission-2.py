a29de6297b04c75a7ce9e909cfaf06a54d6f7df4
kodes-from-zero <khushi.shareeff@gmail.com>
2026-08-25 07:38:41 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s1 = {}
+        t1={}
+        for ch in s:
+            s1[ch]=s1.get(ch,0)+1
+        for ch in t:
+            t1[ch] = t1.get(ch,0)+1
+        return s1 == t1
+        
